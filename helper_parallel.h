ff5ae34cd8cab5369a7302d87e6405b223bcf7aa
yhx <shen_yhx@163.com>
2021-11-04 00:42:46 +0800

    update helper_parallel

--- helper_parallel.h
+++ helper_parallel.h
@@ -4,6 +4,8 @@
 
 #include <mpi.h>
 
+#include "helper_array.h"
+
 template<typename T>
 void mpi_print_array(T *array, size_t size, int rank, int proc_num, const char *msg="")
 {
@@ -16,6 +18,26 @@ void mpi_print_array(T *array, size_t size, int rank, int proc_num, const char *
 		MPI_Barrier(MPI_COMM_WORLD);
 	}
 }
+
+#ifdef USE_GPU
+
+#include "helper_gpu.h"
+
+template<typename T>
+void mpi_print_gpu_array(T *array, size_t size, int rank, int proc_num, const char *msg="")
+{
+	T *tmp = FROMGPU(array, size);
+	for (int i=0; i<proc_num; i++) {
+		if (i == rank) {
+			fprintf(stdout, "%s ", msg);
+			log_array_noendl(stdout, tmp, size);
+			fprintf(stdout, "\n");
+		}
+		MPI_Barrier(MPI_COMM_WORLD);
+	}
+	free_c(tmp);
+}
+#endif
 
 
 #endif // HELPER_PARALLEL_H
